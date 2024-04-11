eea2b6d443469dc2ae2157b5ad9e84b3adc3a894
antspring <max.verzilov32@gmail.com>
2024-04-11 15:39:44 +0500

    feat: added UserLogin model

---
+++ Portfolio/Models/User/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Portfolio.Models.User;
+
+public class UserLogin
+{
+    [Required]
+    [EmailAddress]
+    [JsonPropertyName("email")]
+    public string Email { get; set; }
+    [Required]
+    [JsonPropertyName("password")]
+    public string Password { get; set; }
+}
