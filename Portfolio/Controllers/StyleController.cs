1f89083e7a7959fb30b420106bd22f2ac7f9a729
antspring <max.verzilov32@gmail.com>
2024-06-19 17:35:52 +0500

    feat: added updating Styles

--- Portfolio/Controllers/StyleController.cs
+++ Portfolio/Controllers/StyleController.cs
@@ -1,29 +1,29 @@
 using System.Security.Claims;
 using DataAccess;
-using DataAccess.Models.User;
-using DataAccess.Repositories.Implementations;
+using DataAccess.DTO.Style;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Services.Services;
 
 namespace Portfolio.Controllers
 {
     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
-    public class StyleController(UserRepository userRepository, PortfolioDbContext dbContext) : ControllerBase
+    public class StyleController(StyleService styleService, PortfolioDbContext dbContext) : ControllerBase
     {
         [HttpGet("get")]
         public IActionResult GetStyle()
         {
-            var user = userRepository.WithStyles().GetFirstOrDefault(user =>
-                int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value) == user.Id);
-            if (user.Style == default)
-            {
-                dbContext.Styles.Add(new Style { UserId = user.Id });
-                dbContext.SaveChanges();
-            }
+            var style = styleService.GetStyle(int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value));
+            return Ok(style);
+        }
 
-            return Ok(user.Style);
+        [HttpPut("update")]
+        public IActionResult UpdateStyle(StyleDTO style)
+        {
+            styleService.UpdateStyle(int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value), style);
+            return Ok();
         }
     }
 }
